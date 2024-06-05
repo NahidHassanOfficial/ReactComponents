00124f5c6720d11b4fde9e34459ef2cdd9e57a36
Nahid Hassan <mdnahidhassan01@gmail.com>
2024-06-05 21:07:15 +0600

    images folder moved to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,21 @@ import ScrollComponent1 from "./components/ScrollComponents1";
 const App = () => {
   return (
     <>
-      <div className="ScrollableComponent flex flex-wrap justify-between bg-slate-800 py-4">
+      <div className="ScrollableComponent flex flex-wrap justify-between bg-slate-800 p-4">
         <ScrollComponent
-          imgUrl="../src/assets/images/dummy-img1.jpg"
+          imgUrl="../public/images/dummy-img1.jpg"
           serviceType="Security Service"
         />
         <ScrollComponent1
-          imgUrl="../src/assets/images/url-shortner.webp"
+          imgUrl="../public/images/url-shortner.webp"
           serviceType="Utility Service"
         />{" "}
         <ScrollComponent1
-          imgUrl="../src/assets/images/blogr-landing.jpg"
+          imgUrl="../public/images/blogr-landing.jpg"
           serviceType="Blog Forum"
         />
         <ScrollComponent
-          imgUrl="../src/assets/images/dummy-img1.jpg"
+          imgUrl="../public/images/dummy-img1.jpg"
           serviceType="Security Service"
         />
       </div>
